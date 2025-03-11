1c7bbc6376d2982387f91f0513d424e5eaa59398
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-11 14:19:08 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -104,21 +104,21 @@ loop.run_until_complete(load_sudoers())
 
 if not SESSION_STRING:
     app2 = Client(
-        name="sessions/userbot",
+        name="userbot",
         api_id=API_ID,
         api_hash=API_HASH,
         phone_number=PHONE_NUMBER,
     )
 else:
     app2 = Client(
-        name="sessions/userbot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING
+        name="userbot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING
     )
 
 aiohttpsession = ClientSession()
 
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-app = Client("sessions/wbb", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+app = Client("wbb", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
 log.info("Starting bot client")
 app.start()
